9c42cf239ff2f47b1acc600eaed33fb11fd116dd
m0rp3x <yurash612@gmail.com>
2022-01-14 01:53:29 +0300

    ХАХАХАХАХАХААХ ОНО КОМИТИТЬСЯ

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace aye
+
+{
+
+    internal static class ZalupaHumana
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Фамилию:");
+            var f = Console.ReadLine();
+            Console.WriteLine("Введите Имя");
+            var n = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст");
+            var ages = Console.ReadLine();
+
+            var age = int.Parse(ages ?? string.Empty);
+
+            Console.WriteLine();
+            
+            Console.WriteLine();
+            
+            
+            
+        
+
+            
+
+            Console.WriteLine($"Ваша Фамилия: {f}");
+            Console.WriteLine($"Ваше Имя: {n}");
+            Console.WriteLine($"Вам: {age}");
+            Console.WriteLine("Вы: " + (age > 50 ? "Старый" : "Молодой"));
+
+          
+
+        }
+    }
+}
