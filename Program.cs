23991837ff2be87e9a21d6a366ff0c836ec5aebe
m0rp3x <yurash612@gmail.com>
2022-02-19 00:59:38 +0300

    Я ПОЛУЧИЛ ВЛАСТЬ

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Threading;
 using MySql.Data.MySqlClient;
 
@@ -12,24 +13,26 @@ namespace aye
         public static void Main(string[] args)
         {
 
-            Console.WriteLine("Введите cвоё имя и фамилию:");
+            Console.WriteLine("Введите cвоё имя и фамилию: ");
             var fio = Console.ReadLine();
-            Console.WriteLine("Введите ваш возраст");
+            Console.WriteLine("Введите ваш возраст: ");
             var ages = Console.ReadLine();
             var age = int.Parse(ages ?? string.Empty);
-            Console.WriteLine("Введите ваш вес");
+            Console.WriteLine("Введите ваш вес ");
             var weight = Console.ReadLine();
             var weightint = int.Parse(weight ?? String.Empty);
-            Console.WriteLine("Введите ваш рост");
+            Console.WriteLine("Введите ваш рост ");
             var height = Console.ReadLine();
             double heightint = int.Parse(height ?? String.Empty);
             double bmi = weightint / (heightint % 100 * heightint % 100);
+            Console.WriteLine("Введите id");
+            var id = int.Parse(Console.ReadLixne());
 
             Console.WriteLine();
             Console.WriteLine();
 
             Console.WriteLine(
-                $" Ваш профиль \n Вас зовут {fio}\n Ваш возраст {age}\n Ваш вес {weightint} \n Ваш рост {heightint} \n Ваше ИМТ {bmi}  ");
+                $" Ваш профиль \n Вас зовут {fio}\n Ваш возраст {age}\n Ваш вес {weightint} \n Ваш рост {heightint} \n Ваше ИМТ {bmi} \n Ваш id {id}  ");
 
             Console.WriteLine();
             Console.WriteLine("Далее мы узнаем учитесь вы сейчас или работаете и проведем проф ориентационный тест ");
@@ -39,7 +42,7 @@ namespace aye
 
 
             Console.WriteLine("Происходит загрузка");
-
+            /*
             for (int i = 10; i < 100; i++)
             {
 
@@ -47,6 +50,7 @@ namespace aye
                 Console.WriteLine(i + 1 + "%");
 
             }
+            */
 
             Console.WriteLine("Загрузка успешно завершена");
 
@@ -61,19 +65,39 @@ namespace aye
             Console.WriteLine();
 
 
+            Console.WriteLine("Введите имя вашей базы \n");
+            
+            
+            string Database_name = Console.ReadLine();
+            
+            Console.WriteLine("Введите логин от вашей базы \n  ");
+            
+            string db_log = Console.ReadLine();
+            
+            Console.WriteLine("Пароль от базы данных \n ");
+            
+            string db_pass = Console.ReadLine();
+
+            Console.WriteLine($"имя базы:{Database_name}; логин от базы:{db_log}, пароль:{db_pass} \n");
+
+            Console.WriteLine();
+           
+
+            
+
 
 
             {
 
                 {
 
-                    string constr = "Server=185.12.94.106;Database =2p2s10;Uid=2p2s10;pwd=231-429-617;charset=utf8";
+                    string constr = $"Server=185.12.94.106;Database ={Database_name};Uid={db_log};pwd={db_pass};charset=utf8";
                     MySqlConnection mycon = new MySqlConnection(constr);
 
 
 
                     string sqlExpression =
-                        "INSERT INTO user (fio, age, weight , heint, bmi) VALUES(@fio, @age,@weight, @height, @bmi);";
+                        "INSERT INTO user (id,fio, age, weight , heint, bmi) VALUES(@id,@fio, @age,@weight, @height, @bmi);";
 
                     MySqlCommand command = new MySqlCommand(sqlExpression, mycon);
                     using (MySqlConnection connection = new MySqlConnection(constr))
@@ -86,11 +110,13 @@ namespace aye
                             MySqlParameter nameParam3 = new MySqlParameter("@height", heightint);
                             MySqlParameter nameParam4 = new MySqlParameter("@weight", weightint);
                             MySqlParameter nameParam5 = new MySqlParameter("@bmi", bmi);
+                            MySqlParameter nameParam6 = new MySqlParameter("@id", id);
                             command.Parameters.Add(nameParam);
                             command.Parameters.Add(nameParam2);
                             command.Parameters.Add(nameParam3);
                             command.Parameters.Add(nameParam4);
                             command.Parameters.Add(nameParam5);
+                            command.Parameters.Add(nameParam6);
                             MySqlDataReader reader = command.ExecuteReader();
                             
                             
@@ -99,6 +125,10 @@ namespace aye
                             MySqlConnection conn = new MySqlConnection(constr2);
                             
                             conn.Open();
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            
                             
                             string sql = "SELECT * FROM user";
                             
