d10d5614a32287378daf3e5e4d6625ebcb368828
m0rp3x <67301219+m0rp3x@users.noreply.github.com>
2022-02-24 12:40:54 +0300

    абоба

--- Program.cs
+++ Program.cs
@@ -1,34 +1,29 @@
 using System;
-using System.Runtime.InteropServices;
 using System.Threading;
 using MySql.Data.MySqlClient;
 
-
-namespace aye
+namespace ConsoleApp10
 
 {
 
-    internal class ZalupaHumana
+    internal static class ZalupaHumana
     {
         public static void Main(string[] args)
         {
 
             Console.WriteLine("Введите cвоё имя и фамилию: ");
-            var fio = Console.ReadLine();
+            var fio = (Console.ReadLine());
             Console.WriteLine("Введите ваш возраст: ");
-            var ages = Console.ReadLine();
-            var age = int.Parse(ages ?? string.Empty);
+            var age = int.Parse(Console.ReadLine() ?? string.Empty );
             Console.WriteLine("Введите ваш вес ");
-            var weight = Console.ReadLine();
-            var weightint = int.Parse(weight ?? String.Empty);
+            var weightint = int.Parse(Console.ReadLine() ?? String.Empty);
             Console.WriteLine("Введите ваш рост ");
-            var height = Console.ReadLine();
-            double heightint = int.Parse(height ?? String.Empty);
+            double heightint = int.Parse(Console.ReadLine() ?? String.Empty);
             double bmi = weightint / (heightint % 100 * heightint % 100);
             Console.WriteLine("Введите id");
-            var id = int.Parse(Console.ReadLine());
+            var id = int.Parse(Console.ReadLine() ?? String.Empty);
+            
 
-            Console.WriteLine();
             Console.WriteLine();
 
             Console.WriteLine(
@@ -37,12 +32,16 @@ namespace aye
             Console.WriteLine();
             Console.WriteLine("Далее мы узнаем учитесь вы сейчас или работаете и проведем проф ориентационный тест ");
             Console.WriteLine("Для продолжения нажмите любую клавишу...");
-            Console.ReadKey();
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar){}
+            
+
+              
             Console.WriteLine();
+            
 
 
             Console.WriteLine("Происходит загрузка");
-            /*
+            
             for (int i = 10; i < 100; i++)
             {
 
@@ -50,7 +49,7 @@ namespace aye
                 Console.WriteLine(i + 1 + "%");
 
             }
-            */
+            
 
             Console.WriteLine("Загрузка успешно завершена");
 
@@ -68,17 +67,17 @@ namespace aye
             Console.WriteLine("Введите имя вашей базы \n");
             
             
-            string Database_name = Console.ReadLine();
+            string databaseName = Console.ReadLine();
             
             Console.WriteLine("Введите логин от вашей базы \n  ");
             
-            string db_log = Console.ReadLine();
+            string dblog = Console.ReadLine();
             
             Console.WriteLine("Пароль от базы данных \n ");
             
-            string db_pass = Console.ReadLine();
+            string dbPass = Console.ReadLine();
 
-            Console.WriteLine($"имя базы:{Database_name}; логин от базы:{db_log}, пароль:{db_pass} \n");
+            Console.WriteLine($"имя базы:{databaseName}; логин от базы:{dblog}, пароль:{dbPass} \n");
 
             Console.WriteLine();
            
@@ -90,17 +89,14 @@ namespace aye
             {
 
                 {
-
-                    string constr = $"Server=185.12.94.106;Database ={Database_name};Uid={db_log};pwd={db_pass};charset=utf8";
-                    MySqlConnection mycon = new MySqlConnection(constr);
-
+                    MySqlConnectionStringBuilder constr = new MySqlConnectionStringBuilder($"Server=185.12.94.106;Database ={databaseName};Uid={dblog};pwd={dbPass};charset=utf8");
 
 
                     string sqlExpression =
-                        "INSERT INTO user (id,fio, age, weight , heint, bmi) VALUES(@id,@fio, @age,@weight, @height, @bmi);";
+                        "INSERT INTO user (id,fio, age, weight , heint, bmi, age_stat) VALUES(@id,@fio, @age,@weight, @height, @bmi,@age_stat);";
 
-                    MySqlCommand command = new MySqlCommand(sqlExpression, mycon);
-                    using (MySqlConnection connection = new MySqlConnection(constr))
+                    MySqlCommand command;
+                    using (MySqlConnection connection = new MySqlConnection(constr.ToString()))
                     {
                         using (command = new MySqlCommand(sqlExpression, connection))
                         {
@@ -111,19 +107,21 @@ namespace aye
                             MySqlParameter nameParam4 = new MySqlParameter("@weight", weightint);
                             MySqlParameter nameParam5 = new MySqlParameter("@bmi", bmi);
                             MySqlParameter nameParam6 = new MySqlParameter("@id", id);
+                            MySqlParameter nameParam7 = new MySqlParameter("@age_stat", ageStat);
                             command.Parameters.Add(nameParam);
                             command.Parameters.Add(nameParam2);
                             command.Parameters.Add(nameParam3);
                             command.Parameters.Add(nameParam4);
                             command.Parameters.Add(nameParam5);
                             command.Parameters.Add(nameParam6);
-                            MySqlDataReader reader = command.ExecuteReader();
+                            command.Parameters.Add(nameParam7);
+                            MySqlDataReader reader;
+                            command.ExecuteReader();
+
+
+                            MySqlConnection conn = new MySqlConnection(constr.ToString());
                             
-                            
-                            string constr2 = $"Server=185.12.94.106;Database ={Database_name};Uid={db_log};pwd={db_pass};charset=utf8";                          
-                            MySqlConnection conn = new MySqlConnection(constr2);
-                            
-                            conn.Open();
+                            conn.Open();    
                             Console.WriteLine();
                             Console.WriteLine();
                             Console.WriteLine();
@@ -138,11 +136,17 @@ namespace aye
                             while (reader.Read())
                             {
                                
-                                Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString());
+                                Console.WriteLine(reader[0] + " " + reader[1]);
                             }
                             reader.Close(); 
                             conn.Close();
+
+                            Console.WriteLine("Вас соскамили в базу хихихаха");
+                                
                             
+
+
+
 
 
 
